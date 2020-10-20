ef3e3bb20c6f6546ff5109a7ba97c072c2fc4c2f
Kartik <gautam.kartik05@gmail.com>
2020-10-20 20:20:02 +0530

    initial commit

---
+++ tiktak.py
@@ -0,0 +1,30 @@
+# Python Tik Tak Toe Game
+
+# Array of responses
+arr = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+
+# PlayBoard display
+def printPlayBoard():
+    print("\n")
+    print(' '+arr[0][0]+' | '+arr[0][1]+' | '+arr[0][2])
+    print("-----------")
+    print(' '+arr[1][0]+' | '+arr[1][1]+' | '+arr[1][2])
+    print("-----------")
+    print(' '+arr[2][0]+' | '+arr[2][1]+' | '+arr[2][2])
+    print("\n")
+
+def getInput(sym):
+    inp = input("Enter the matric position : ")
+    assignInput(inp[0],inp[1],sym)
+
+def assignInput(row,col,sym):
+    arr[row][col]=sym
+
+
+def driver():
+    
+
+getInput('x')
+printPlayBoard()
+
+
