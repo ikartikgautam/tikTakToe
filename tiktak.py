1fe6fe925d24a263c09af3393e7eccebedf198ba
Kartik <gautam.kartik05@gmail.com>
2020-10-23 17:30:59 +0530

    comments added

--- tiktak.py
+++ tiktak.py
@@ -1,8 +1,28 @@
 # Python Tik Tak Toe Game
+
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+# ------------------------ IMPORTS --------------------------------
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+
 import random
+
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+# ----------------------- VARIABLES -------------------------------
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
 
 # Array of responses
 arr = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+
+
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+# ------------------ FUNCTION DEFINITIONS -------------------------
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
 
 
 # PlayBoard display
@@ -15,6 +35,7 @@ def printPlayBoard():
     print(' '+arr[2][0]+' | '+arr[2][1]+' | '+arr[2][2])
     print("\n")
 
+# Get input from user
 def getInput(sym):
     inp = input("Enter the matric position : ")
     if(pingPlayBoard(inp[0],inp[1])):
@@ -27,14 +48,17 @@ def getInput(sym):
         print("Already occupied.. Retry !")
         getInput(sym)
 
+# Assign input to array
 def assignInput(row,col,sym):
     arr[row][col]=sym
 
+# Check if the playboard slot is empty or not
 def pingPlayBoard(row,col):
     if(arr[row][col]!=' '):
         return False
     return True            
 
+# Driver for multi player game
 def manualDriver():
     for e in range(0,9):
         if(e%2==0):
@@ -43,6 +67,7 @@ def manualDriver():
             getInput('0')
     print("Its a Draw !!!!!!!    : (")
 
+# Check winner
 def checkWinner(sym):
     if((arr[0][0]==sym and arr[0][1]==sym and arr[0][2]==sym) or (arr[1][0]==sym and arr[1][1]==sym and arr[1][2]==sym) or (arr[2][0]==sym and arr[2][1]==sym and arr[2][2]==sym)):
         return True
@@ -53,6 +78,7 @@ def checkWinner(sym):
     else:
         return False
 
+# Driver function for single player game
 def autoDriver():
     mySym = raw_input("choose your symbol from 'x' or '0' : ")
     comp = ' '
@@ -70,6 +96,7 @@ def autoDriver():
 
     print("Its a Draw !!!!!!!    : (")
 
+# Get input in singleplayer game
 def getCompInput(sym):
     row = random.randint(0,2)
     col = random.randint(0,2)
@@ -82,10 +109,17 @@ def getCompInput(sym):
     else:
         getCompInput(sym)
 
+# Init function / Entry function
 def init():
     if(input("Do you want to play multiplayer or with computer ? 1 = Computer / 2 = Multiplayer ")==1):
         autoDriver()
     else:
         manualDriver()
+
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+# --------------------- PROGRAM START -----------------------------
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
 
 init()
